4b08e05018490623a36a8d473000f92075f496a8
millieTime <thisismynewemailsoyeah@gmail.com>
2021-02-09 18:20:06 -0700

    merged apis together

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import pickle
 import random
 import flask
 from flask import request, send_file, jsonify
+from flask_cors import CORS
 
 #variable initialization for spotify model
 #-----------------------------------------------------------------------------#
@@ -33,6 +34,7 @@ redirect_uri ='https://www.attentioncoach.es/'   #@param {type: "string"}
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -59,6 +61,34 @@ def requestSimilarPlaylist():
                                  [creativity, 1-creativity], [request.args['song']], tracks, track_ids, \
                                  size=size, lookback=lookback, noise=noise, replace=replace)})
 
+# Music Player Stuff
+@app.route('/v1/test-song-player/')
+def generate_audioplayer_page():
+    try:
+        song_list = ["8-bit.wav", "Hunter_Fav.wav", "piano.wav"]
+        song_choice = random.choice(song_list)
+        path = "audio\\" + song_choice
+        print("asking file", path)
+        with open("Audioplayerpt1.txt", "r") as first_half, open("Audioplayerpt2.txt", "r") as second_half:
+            return (first_half.read() + f'<audio controls id="myAudio" src="http://localhost:5000/v1/return-file?path={path}"'
+            + f' crossorigin="anonymous" autoplay></audio><p id="currentSong">{song_choice}' + second_half.read())
+    except Exception as e:
+        return str(e)
+
+@app.route('/v1/return-file', methods=['GET'])
+def return_files():
+    # request.args is all the &__=__&**=__&^^=__
+    query_parameters = request.args
+    path = query_parameters.get('path')
+    print("returning file", path)
+    try:
+        return send_file(path)
+    except Exception as e:
+        return str(e)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 #-----------------------------------------------------------------------------#
 
 
